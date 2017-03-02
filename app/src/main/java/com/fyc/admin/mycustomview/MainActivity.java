0052903ce01008df8f504a2000a7c3016ff8c980
981884394@qq.com <20984591606>
2017-03-02 20:48:31 +0800

    Initial commit

---
+++ app/src/main/java/com/fyc/admin/mycustomview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fyc.admin.mycustomview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppC{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
